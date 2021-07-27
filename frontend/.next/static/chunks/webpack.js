1ae1d01167f3c97a04b89d7dd4b7fb7f24ae48e7
Florence Anipa <mawuaniipa@gmail.com>
2021-07-27 12:23:27 +0300

    Fix style components

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "626d2516373707b96dd6"; }
+/******/ 		__webpack_require__.h = function() { return "e90b81eb0a9b3b658e01"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
